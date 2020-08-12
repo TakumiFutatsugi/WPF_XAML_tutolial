7e94667c7aaf39e4d8a4a4b48702475efd39acc8
takumi_futatsugi <takumi_futatsugi@sobal.co.jp>
2020-08-12 15:32:26 +0900

    UI配置 XAML作成 sliderのデータバインディング

---
+++ WpfColorController/WpfColorController/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfColorController
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public class ColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            byte R = (byte)(double)(values[0]);
+            byte G = (byte)(double)(values[1]);
+            byte B = (byte)(double)(values[2]);
+
+            return Color.FromRgb(R, G, B);
+        }
+
+        //-----------------------------------------------------------------------------------------------
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    } // end of ColorConverter class
+}
